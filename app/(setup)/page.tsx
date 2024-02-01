d238eac1b983bb854540d525b6e75c7774d6bdff
Nikita <nikitatimoshin23@gmail.com>
2024-02-01 18:48:03 +0300

    initial commit & prisma setup

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,22 @@
+import { db } from "@/lib/db";
+import { initiateProfile } from "@/lib/initiate-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+  const profile = await initiateProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) return redirect(`/servers/${server.id}`);
+
+  return <div>Create a new Server</div>;
+};
+
+export default SetupPage;
