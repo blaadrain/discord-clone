ec00a5f843a2c385db472e1a0f8a5b5cca89055c
Nikita <nikitatimoshin23@gmail.com>
2024-02-08 14:46:31 +0300

    feat: implemented socket io setup

--- components/chat/header.tsx
+++ components/chat/header.tsx
@@ -1,5 +1,7 @@
-import { Hash } from "lucide-react";
+import { MessagesSquare } from "lucide-react";
 import { MobileToggle } from "../mobile-toggle";
+import { UserAvatar } from "../user-avatar";
+import { SocketIndicator } from "../socket-indicator";
 
 type ChatHeaderProps = {
   serverId: string;
@@ -18,9 +20,15 @@ export const ChatHeader = ({
     <div className="text-md flex h-12 items-center border-b-2 border-zinc-300 px-3 font-semibold dark:border-zinc-700">
       <MobileToggle serverId={serverId} />
       {type === "channel" && (
-        <Hash className="mr-2 h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+        <MessagesSquare className="mr-2 h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+      )}
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="mr-2 h-8 w-8 md:h-8 md:w-8" />
       )}
       <p className="text-md font-semibold text-black dark:text-white">{name}</p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
